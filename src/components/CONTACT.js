300cc7e2dd461aee2ff427e855a64957c980b181
tonui123 <to.asiatique@gmail.com>
2020-08-21 20:00:41 +0700

    first commit

---
+++ src/components/CONTACT.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CONTACT extends Component {
+    render() {
+        return (
+            <div>
+                <h2>ติดต่อเรา</h2>
+                <p>87 ซ.รัชฎภัณฑ์ ถ.ราชปรารภแขวง มักกะสัน เขตราชเทวี กทม. 10400</p>
+            </div>
+        )
+    }
+}
